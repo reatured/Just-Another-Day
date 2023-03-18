14dc12f194587c8592f64e7ee7f17d11f561d782
reatured <61943376+reatured@users.noreply.github.com>
2023-03-18 00:18:30 -0700

    Need to add paint brush

--- Assets/AllGameScenes/Level 3/Scripts/Painting_L3.cs
+++ Assets/AllGameScenes/Level 3/Scripts/Painting_L3.cs
@@ -1,52 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class Painting_L3 : MonoBehaviour
 {
     public Texture2D[] layers;
     public GameObject paintingPaper;
     public Texture2D paintingTexture;
-    public Material paintingMaterial;
+    public Material paintingPaper_Mat;
 
-    public Vector3 contactPoint; 
+    public Vector3 contactPoint;
+
+
+    public Texture2D colorMap_Texture; 
+    public float colorStep; 
+
     // Start is called before the first frame update
     void Start()
     {
         getPaperSize();
-        paintingMaterial = paintingPaper.GetComponent<MeshRenderer>().materials[0];
+        paintingPaper_Mat = paintingPaper.GetComponent<MeshRenderer>().materials[0];
+
+        colorStep = 40/255f -0.01f;
+
+        for (int i = 1; i <= layers.Length; i++)
+        {
+            string id = "Mask_" + i;
+            paintingPaper_Mat.SetTexture(id, layers[i - 1]);
+            id = "Color_" + i;
+
+            paintingPaper_Mat.SetColor(id, Color.white);
+        }
         //paintingTexture = new Texture2D(3300, 2550);
 
-        //for (int y = 0; y < paintingTexture.height; y++)
+        //for (int y = 0; y < colorMap_Texture.height; y++)
         //{
-        //    for (int x = 0; x < paintingTexture.width; x++)
+        //    for (int x = 0; x < colorMap_Texture.width; x++)
         //    {
-        //        Color color = layers[1].GetPixel(x, y);
-        //        Color original = paintingTexture.GetPixel(x, y);
-        //        paintingTexture.SetPixel(x, y, Color.Lerp(color, original, 0.5f));
+
+        //        Color pixelColor = new Color(0,0,0,0);
+        //        for(int i =  0; i < layers.Length; i++)
+        //        {
+        //            int _x = x / colorMap_Texture.width * layers[i].width;
+        //            int _y = y / colorMap_Texture.height * layers[i].height;
+        //            pixelColor += layers[i].GetPixel(_x, _y);
+
+        //        }
+        //        colorMap_Texture.SetPixel(x, y, pixelColor) ;
         //    }
         //}
-        //paintingTexture.Apply();
+        //colorMap_Texture.Apply();
+        //colorMap_mat.SetTexture("Mask_0", colorMap_Texture);
 
-        paintingMaterial.SetTexture("_Texture2D", paintingTexture);
-        paintingMaterial.SetVector("_ClickedPos", new Vector2(1, 1));
+        //print(paintingTexture.GetPixel(20, 20));
+
+        //paintingMaterial.SetTexture("_Texture2D", paintingTexture);
+        paintingPaper_Mat.SetVector("_ClickedPos", new Vector2(1, 1));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    Vector2 uv = Vector2.zero; 
+    Vector2 uv = Vector2.zero;
     private void OnMouseDown()
     {
         contactPoint = getImpactPoint();
-        uv.x = Vector3.Dot(contactPoint - b.position, widthVec);
-        uv.y = Vector3.Dot(contactPoint - b.position, lengthVec);
+        Vector3 contactVec = contactPoint - b.position;
+        uv.x = Vector3.Dot(contactVec, widthVec) / Vector3.SqrMagnitude(widthVec);
+        uv.y = Vector3.Dot(contactVec, lengthVec) / Vector3.SqrMagnitude(lengthVec);
+        print(contactVec);
         print(uv);
 
+        float positionValue = colorMap_Texture.GetPixel((int)(uv.x * colorMap_Texture.width), (int)(uv.y * colorMap_Texture.height)).r;
+        print(positionValue);
+        
+
+        int colorBlock = (int)(positionValue / colorStep);
+        print(colorBlock);
     }
 
 
@@ -78,7 +103,8 @@ public class Painting_L3 : MonoBehaviour
 
     public Vector3 lengthVec, widthVec;
 
-    public void getPaperSize() {
+    public void getPaperSize()
+    {
         length = Vector3.Distance(a.position, b.position);
         width = Vector3.Distance(b.position, c.position);
         lengthVec = a.position - b.position;
