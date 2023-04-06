ac833c35440f85bca8773b120328877e3d4cd33b
reatured <61943376+reatured@users.noreply.github.com>
2023-04-06 01:37:29 -0400

    Small Update

---
+++ Assets/_AllGameScenes/Level 2/Scripts/QuickFix_Needle_L2_V3.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickFix_Needle_L2_V3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        Cursor.visible = false;
+    }
+}
